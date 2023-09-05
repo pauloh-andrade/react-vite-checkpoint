4e5fd7b661e28e1b6e579d7e7d16c6e095854c0f
Paulo Andrade <paulo.andrade@accurate.com.br>
2023-09-05 13:29:35 -0300

    added product update

--- src/routes/UpdateProducts/index.jsx
+++ src/routes/UpdateProducts/index.jsx
@@ -1,9 +1,22 @@
+import { useParams } from 'react-router-dom'
+
 import './updateProducts.css'
+import productList from '../../mocks/productList'
 
 const UpdateProducts = () => {
+  const { id } = useParams()
+
+  const product = productList.filter((prod) => {
+    if (prod.id == id) {
+      return prod
+    }
+  })
   return (
     <main>
       <h1>Editando o produto</h1>
+      <h2>Produto</h2>
+      <p>Nome : {product[0].name}</p>
+      <p>Preço : {product[0].price}</p>
     </main>
   )
 }
